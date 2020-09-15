3e906a82b87c76ce4437a202bbb61665cb7be784
avacallery <avacallery@gmail.com>
2020-09-15 11:13:00 -0400

    Bank classes

--- src/com/avacallery/bank/CheckingAccount.java
+++ src/com/avacallery/bank/CheckingAccount.java
@@ -1,9 +1,7 @@
 package com.avacallery.bank;
 
 public class CheckingAccount extends BankAccount {
-    // because our parent account has a constructor, we need a constructor here
-    public CheckingAccount(int balance, int accountNum, String owner){
+    public CheckingAccount(int balance, int accountNum, String owner) {
         super(balance, accountNum, owner, "Checking");
     }
-
 }
--- src/com/avacallery/bank/SavingsAccount.java
+++ src/com/avacallery/bank/SavingsAccount.java
@@ -1,15 +1,10 @@
 package com.avacallery.bank;
 
 public class SavingsAccount extends BankAccount {
-
-    // instead of const, we use static bc it does not change
     static int withdrawLimit = 5;
     private int withdrawCount;
     private int interest;
-    // 2% will be stored as 2
 
-    // because our parent account has a constructor, we need a constructor here
-    // and we must use super to bring the attributes in
     public SavingsAccount(int balance, int accountNum, String owner, int interest) {
         super(balance, accountNum, owner, "Savings");
         this.withdrawCount = 5;
@@ -17,8 +12,6 @@ public class SavingsAccount extends BankAccount {
     }
 
     public void applyInterest() {
-        // in order to get a decimal back
-        balance += balance * ((float) interest / 100.0);
+        deposit((int)(balance * ((float) interest / 100.0)));
     }
-
 }
